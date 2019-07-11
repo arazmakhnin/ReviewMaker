5c4e00d9c77348780493ad87a21df58bb32acb35
Anton Razmakhnin <anton.razmakhnin@devfactory.com>
2019-07-12 02:02:08 +0400

    Sln file version changed

--- Program.cs
+++ Program.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
-using Atlassian.Jira;
-using Google.Apis.Services;
-using Google.Apis.Sheets.v4.Data;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace ReviewMaker
 {
